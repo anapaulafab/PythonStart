4edbfd84934c2e6f9a2f9a3f0ed6b0db5951d6ec
Ana Paula Fabrini <aninhafafipa@gmail.com>
2020-05-05 16:01:19 -0300

    Usando input

---
+++ Soma.py
@@ -0,0 +1,8 @@
+n1 = input ('Primeiro número ')
+n2 = input ('Segundo número ')
+soma =  int (n1) + int (n2)
+print( ' A soma é  ', soma )
+
+n1 = int(input ('Primeiro número '))
+n2 = int(input ('Segundo número '))
+print( ' A soma é  ', (n1+n2) )
