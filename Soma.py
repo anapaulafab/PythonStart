4cd20d483de0dbd9fd80802937c1cb700c64c5f2
Ana Paula Fabrini <aninhafafipa@gmail.com>
2020-05-05 16:05:26 -0300

    Aumento do espaço

--- Soma.py
+++ Soma.py
@@ -3,6 +3,7 @@ n2 = input ('Segundo número ')
 soma =  int (n1) + int (n2)
 print( ' A soma é  ', soma )
 
+
 n1 = int(input ('Primeiro número '))
 n2 = int(input ('Segundo número '))
 print( ' A soma é  ', (n1+n2) )
