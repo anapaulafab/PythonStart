fe4ec7afdda6e99c74ed5b91e82d7630f8b85a0a
Ana Paula Fabrini <aninhafafipa@gmail.com>
2020-05-28 17:15:04 -0300

    Aula sobre tipos primitivos e Resolução dos desafios 03 e 04.

---
+++ TiposPrimitivos.py
@@ -0,0 +1,10 @@
+n1= int (input ('Digite um valor: '))
+n2 = int (input ('Digite outro valor: '))
+s=n1 + n2
+#print( 'A soma é ', s)
+#print ( 'A soma de ', (n1) ,'+' , (n2), 'é' , (s))
+
+# ANTES DA V. 3.7
+#print ('A soma entre {} e {} é {}'.format(n1,n2,s))
+
+print (f'A soma entre {n1} e {n2} é {s}')
---
+++ TiposPrimitivos02.py
@@ -0,0 +1,3 @@
+n = input ('Digite algo:')
+#(type(n))
+print(f' É um numero ? {n.isnumeric()}\n É alfabetico ? {n.isalpha()}\n É alfanumerico ? {n.isalnum()}')
