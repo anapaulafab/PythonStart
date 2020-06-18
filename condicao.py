f406304a256cc0bdc5b6320f2499ed768c86cf72
anapaulafab <aninhafafipa@gmail.com>
2020-06-18 20:19:08 -0300

    Condição if e else

---
+++ condicao.py
@@ -0,0 +1,10 @@
+tempo = int(input('Quantos anos tem seu carro ?: '))
+if tempo <=3:
+    print('Seu carro é novo')
+else:
+    print('Seu carro é velho')
+print('----FIM----') 
+
+tempo = int(input('Quantos anos tem seu carro ?: '))
+print('Seu carro é novo'if tempo <=3 else 'Seu carro é velho')
+print('----FIM----') 
